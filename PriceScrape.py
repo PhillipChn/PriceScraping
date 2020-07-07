94843aa850d6a6f09452928502ed591ec4e9604d
Phillip Chan <chnphillip@gmail.com>
2020-07-07 10:23:51 -0700

    Fixed Error Displaying Wrong Product & Now Appends to CSV

--- PriceScrape.py
+++ PriceScrape.py
@@ -1,6 +1,7 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+from datetime import datetime as date
 
 def microCenter(tableName, site, fileWriter):
 
@@ -37,16 +38,27 @@ def microCenter(tableName, site, fileWriter):
 
 
 def main():
+
+    # datetime object containing current date and time
+    now = date.now()
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
     fileName = "products.csv"
-    file = open(fileName, "w")
-    headers = "Name,Price,Shipping,Available\n"
+    file = open(fileName, "a")
+    file.write("\n\nDate: " + dt_string)
+    headers = "\nNewegg\nName,Price,Shipping,Available\n"
     file.write(headers)
 
     # NewEgg GIGABYTE B450M DS3H AM4 AMD B450 SATA 6Gb/s Micro ATX AMD Motherboard
     NE_Giga = 'https://www.newegg.com/p/pl?d=GIGABYTE%20B450M%20DS3H&N=8000'
+    # MSI B450 TOMAHAWK MAX AM4 AMD B450 SATA 6Gb/s ATX AMD Motherboard
     NE_TM  = 'https://www.newegg.com/p/pl?d=MSI%20B450%20TOMAHAWK%20MAX&N=8000'
+    # MSI B450 GAMING PLUS MAX AM4 AMD B450 SATA 6Gb/s ATX AMD Motherboard
     NE_GPM = 'https://www.newegg.com/p/pl?d=MSI%20B450%20Gaming%20Plus%20MAX&N=8000'
+    # MicroCenter Webpage for AMD Motherboards
     MC_MB = 'https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966996+4294818892&myStore=true'
+    # MicroCenter Webpage for AMD Processors
     MC_PCS = 'https://www.microcenter.com/category/4294966995,4294819840/amd-processors'
 
     urls = []
@@ -70,8 +82,9 @@ def main():
         parser = soup(test, "html.parser")
 
         # Gets the product name
-        container = parser.find("div", {"class":"item-info"})
-        container = container.text[10:]
+        container = parser.find("div", {"class":"items-view is-grid"})
+        print(container.div.a.img['title'])
+
         product_name = container.strip()
 
         # Gets the product price
