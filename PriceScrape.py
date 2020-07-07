9bc980715d901c47257010c28321e5aa1fde8998
Phillip Chan <chnphillip@gmail.com>
2020-07-06 23:36:45 -0700

    Organized Code into Functions for each Website

--- PriceScrape.py
+++ PriceScrape.py
@@ -2,26 +2,14 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-fileName = "products.csv"
-file = open(fileName, "w")
-headers = "Name,Price,Shipping,Available\n"
-file.write(headers)
+def microCenter(tableName, site, fileWriter):
 
-# NewEgg GIGABYTE B450M DS3H AM4 AMD B450 SATA 6Gb/s Micro ATX AMD Motherboard
-NE_Giga = 'https://www.newegg.com/p/pl?d=GIGABYTE%20B450M%20DS3H&N=8000'
-NE_TM  = 'https://www.newegg.com/p/pl?d=MSI%20B450%20TOMAHAWK%20MAX&N=8000'
-NE_GPM = 'https://www.newegg.com/p/pl?d=MSI%20B450%20Gaming%20Plus%20MAX&N=8000'
-
-urls = []
-urls.append(NE_Giga)
-urls.append(NE_TM)
-urls.append(NE_GPM)
-
-
-for url in urls:
+    fileWriter.write("\n\n\n")
+    fileWriter.write("MicroCenter " + tableName + "\n")
+    fileWriter.write("Product Name,Price,Stock\n")
 
     # Opens Website URL & Downloads Page HTML
-    uClient = uReq(url)
+    uClient = uReq(site)
 
     # Store Elements into variable
     test = uClient.read()
@@ -33,54 +21,81 @@ for url in urls:
     parser = soup(test, "html.parser")
 
     # Gets the product name
-    container = parser.find("div", {"class":"item-info"})
-    container = container.text[10:]
-    product_name = container.strip()
+    container = parser.find("ul", {"role": "tabpanel"})
+    product_list = container.findAll("li")
 
-    # Gets the product price
-    container = parser.find("div",{"class":"item-action"})
-    price_box = container.find("li",{"class":"price-current"})
-    price = (price_box.strong.text + price_box.sup.text)
+    i = 0
+    for product in product_list:
+        singleProduct = product_list[i].find("a", {"class": "image"})
+        name = singleProduct['data-brand'] + " " + singleProduct['data-name']
+        fileWriter.write(name + ",")
+        fileWriter.write(singleProduct['data-price'] + ",")
+        container = parser.findAll("div", {"class": "stock"})
+        fileWriter.write(container[i].strong.text + "\n")
+        i += 1
 
-    # Gets the product shipping
-    shipping_box = parser.find("li",{"class":"price-ship"})
-    shipping = shipping_box.text.strip()
 
-    OOS = parser.find("p",{"class":"item-promo"})
-    if OOS.text == "":
-        availability = "In Stock"
-    else:
-        availability = OOS.text
 
-    file.write(product_name.replace(",","|") + "," + price + "," + shipping + "," + availability + "\n")
+def main():
+    fileName = "products.csv"
+    file = open(fileName, "w")
+    headers = "Name,Price,Shipping,Available\n"
+    file.write(headers)
 
-file.write("\n\n\n")
-file.write("MOBO,Price,Stock\n")
+    # NewEgg GIGABYTE B450M DS3H AM4 AMD B450 SATA 6Gb/s Micro ATX AMD Motherboard
+    NE_Giga = 'https://www.newegg.com/p/pl?d=GIGABYTE%20B450M%20DS3H&N=8000'
+    NE_TM  = 'https://www.newegg.com/p/pl?d=MSI%20B450%20TOMAHAWK%20MAX&N=8000'
+    NE_GPM = 'https://www.newegg.com/p/pl?d=MSI%20B450%20Gaming%20Plus%20MAX&N=8000'
+    MC_MB = 'https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966996+4294818892&myStore=true'
+    MC_PCS = 'https://www.microcenter.com/category/4294966995,4294819840/amd-processors'
 
-# Opens Website URL & Downloads Page HTML
-uClient = uReq('https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966996+4294818892&myStore=true')
+    urls = []
+    urls.append(NE_Giga)
+    urls.append(NE_TM)
+    urls.append(NE_GPM)
 
-# Store Elements into variable
-test = uClient.read()
 
-# Close Page Connection
-uClient.close()
+    for url in urls:
 
-# HTML parser
-parser = soup(test, "html.parser")
+        # Opens Website URL & Downloads Page HTML
+        uClient = uReq(url)
 
-# Gets the product name
-container = parser.find("ul", {"role": "tabpanel"})
-product_list = container.findAll("li")
+        # Store Elements into variable
+        test = uClient.read()
 
-i = 0
-for product in product_list:
-    singleProduct = product_list[i].find("a", {"class": "image"})
-    name = singleProduct['data-brand'] + " " + singleProduct['data-name']
-    file.write(name + ",")
-    file.write(singleProduct['data-price'] + ",")
-    container = parser.findAll("div", {"class": "stock"})
-    file.write(container[i].strong.text + "\n")
-    i += 1
+        # Close Page Connection
+        uClient.close()
 
-file.close()
+        # HTML parser
+        parser = soup(test, "html.parser")
+
+        # Gets the product name
+        container = parser.find("div", {"class":"item-info"})
+        container = container.text[10:]
+        product_name = container.strip()
+
+        # Gets the product price
+        container = parser.find("div",{"class":"item-action"})
+        price_box = container.find("li",{"class":"price-current"})
+        price = (price_box.strong.text + price_box.sup.text)
+
+        # Gets the product shipping
+        shipping_box = parser.find("li",{"class":"price-ship"})
+        shipping = shipping_box.text.strip()
+
+        OOS = parser.find("p",{"class":"item-promo"})
+        if OOS.text == "":
+            availability = "In Stock"
+        else:
+            availability = OOS.text
+
+        file.write(product_name.replace(",","|") + "," + price + "," + shipping + "," + availability + "\n")
+
+
+    microCenter(tableName="Motherboards", site=MC_MB, fileWriter=file)
+    microCenter(tableName="Processors", site=MC_PCS, fileWriter=file)
+
+    file.close()
+
+if __name__ == '__main__':
+    main()
