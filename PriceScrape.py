9554ed4e7b8c07a9850cf8dd00353366686520e7
Phillip Chan <chnphillip@gmail.com>
2020-07-06 22:06:58 -0700

    Changed url & got Name

--- PriceScrape.py
+++ PriceScrape.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 # NewEgg GIGABYTE B450M DS3H AM4 AMD B450 SATA 6Gb/s Micro ATX AMD Motherboard
-NE_Giga = 'https://www.newegg.com/gigabyte-b450m-ds3h/p/N82E16813145083?Item=N82E16813145083'
+NE_Giga = 'https://www.newegg.com/p/pl?d=GIGABYTE%20B450M%20DS3H&N=8000'
 
 # Opens Website URL & Downloads Page HTML
 uClient = uReq(NE_Giga)
@@ -14,4 +14,13 @@ test = uClient.read()
 # Close Page Connection
 uClient.close()
 
-print(test)
+# HTML parser
+parser = soup(test, "html.parser")
+
+# container = parser.find("div", {"class":"wrapper"})
+# product_name = container.h1.span.text
+#
+container = parser.find("div", {"class":"item-info"})
+container = container.text[10:]
+product_name = container.strip()
+
